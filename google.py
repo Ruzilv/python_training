60eb68ff8f7f3b581b6d817c4120c72cee76bcbb
Ruzilv <Ruzilv@gmail.com>
2020-04-17 12:18:05 +0500

    Созданы пакеты для разных зон ответственности

--- google.py
+++
@@ -1,5 +0,0 @@
-from selenium import webdriver
-
-# Указываем полный путь к geckodriver.exe на вашем ПК.
-driver = webdriver.Chrome("C:\\chromedriver\\chromedriver.exe")
-driver.get("http://addressbook/")
